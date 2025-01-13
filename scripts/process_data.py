ea7beceeba803770c6e605262790e5a77fb842c5
Feiyang Wu <feiyangw.21@intl.zju.edu.cn>
2025-01-13 19:22:50 +0800

    process data into zarr array finish

--- scripts/process_data.py
+++ scripts/process_data.py
@@ -2,28 +2,27 @@ import os
 import zarr
 import numpy as np
 
-# trial_num = "1"
-# root_path = os.path.expanduser("~") + "/data/trial" + trial_num + "/"
-# output_path = os.path.expanduser("~") + "data/dataset/data" + trial_num
-
 os.system('rm -rf pick_and_place_data.zarr')
 store = zarr.DirectoryStore('pick_and_place_data.zarr')
 root = zarr.group(store=store)
 data_group = root.create_group('data')
 meta_group = root.create_group('meta')
 action_arr = data_group.create_dataset('action', shape=(0,7,), chunks=(100, 7), dtype='f4')
-img1_arr = data_group.create_dataset('img1', shape = (0,3,480,640), chunks = (10, 3, 480, 640), dtype='f4')
-img2_arr = data_group.create_dataset('img2', shape = (0,3,480,640), chunks = (10, 3, 480, 640), dtype='f4')
+img1_arr = data_group.create_dataset('img1', shape = (0,480,640,3), chunks = (10, 480, 640, 3), dtype='f4')
+img2_arr = data_group.create_dataset('img2', shape = (0,480,640,3), chunks = (10, 480, 640, 3), dtype='f4')
 img1_depth_arr = data_group.create_dataset('depth1', shape = (0,480,640), chunks = (10, 480, 640), dtype='f4')
 img2_depth_arr = data_group.create_dataset('depth2', shape = (0,480,640), chunks = (10, 480, 640), dtype='f4')
 joint_pos_arr = data_group.create_dataset('joint_pos', shape = (0,7,), chunks = (100, 7), dtype='f4')
 joint_vel_arr = data_group.create_dataset('joint_vel', shape = (0,7,), chunks = (100, 7), dtype='f4')
-joint_torque_arr = data_group.create_dataset('joint_torque', shape = (0,7,), chunks = (100, 7), dtype='f4')
+joint_torque_arr = data_group.create_dataset('joint_torque', shape = (0,20,7), chunks = (100,20,7), dtype='f4')
+episode_end_arr = meta_group.create_dataset('episode_end', shape = (0,), chunks = (100, ), dtype='i4')
 
 episode_end_idx = []
+acc_episode_len = 0
 
-max_episode = 1
+max_episode = 2
 data_idx = 0
+
 for episode_num in range(1, max_episode+1):
     dir_path = os.path.expanduser("~") + "/data/trial" + str(episode_num) + "/"
     cnt = 0
@@ -56,56 +55,67 @@ for episode_num in range(1, max_episode+1):
             elif file[0:4] == "cam2" and file.endswith("_depth.npy"):
                 cam2_depth_file.append(file)
 
+    # Update episode index (does not count last image)
+    episode_len = len(cam1_file) - 1
+    acc_episode_len += episode_len
+    episode_end_idx.append(acc_episode_len)
+    episode_end_arr.resize((episode_end_arr.shape[0]+1))
+    episode_end_arr[-1] = acc_episode_len
 
+    # Sort the files
     cam1_file.sort()
     cam2_file.sort()
     cam1_depth_file.sort()
     cam2_depth_file.sort()
+
     # Add image to the zarr (get rid of the last image)
-    for i in range(len(cam1_file) - 1):
+    for i in range(episode_len):
         img1_arr.resize((img1_arr.shape[0]+1, img1_arr.shape[1], img1_arr.shape[2], img1_arr.shape[3]))
         img1 = np.load(dir_path + cam1_file[i])
+        img1_arr[img1_arr.shape[0]-1] = zarr.array(img1)
+
         img2_arr.resize((img2_arr.shape[0]+1, img2_arr.shape[1], img2_arr.shape[2], img2_arr.shape[3]))
         img2 = np.load(dir_path + cam2_file[i])
-        # img1_arr[img1_arr.shape[0]-1] = zarr.array(img1)
-    
-    # Add to the action and joint_pos zarr array
-    # action_arr.resize((action_arr.shape[0]+1, action_arr.shape[1]))
-    # action_arr[action_arr.shape[0]-1] = zarr.array(joint_pos)
+        img2_arr[img1_arr.shape[0]-1] = zarr.array(img2)
 
-    # joint_pos_arr.resize((joint_pos_arr.shape[0]+1, joint_pos_arr.shape[1]))
-    # joint_pos_arr[joint_pos_arr.shape[0]-1] = zarr.array(joint_pos)
+        img1_depth_arr.resize((img1_depth_arr.shape[0]+1, img1_depth_arr.shape[1], img1_depth_arr.shape[2]))
+        img1_depth = np.load(dir_path + cam1_depth_file[i])
+        img1_depth_arr[img1_arr.shape[0]-1] = zarr.array(img1_depth)
 
-    
+        img2_depth_arr.resize((img2_depth_arr.shape[0]+1, img2_depth_arr.shape[1], img2_depth_arr.shape[2]))
+        img2_depth = np.load(dir_path + cam2_depth_file[i])
+        img2_depth_arr[img2_depth_arr.shape[0]-1] = zarr.array(img2_depth)
+
+    # Map Image to joint information
+    diff = cam1_time - cam2_time    # Make sure cam1_time and cam2_time has the same dimension
+    i = 0
+    arm_idx = []
+    for j in range(episode_len):
+        while i < arm_time.shape[0]:
+            if np.abs(cam1_time[j]-arm_time[i]) < 0.01:
+                # print("%f, %f, %f"%(cam1_time[j], arm_time[i], arm_time[i+20]))
+                arm_idx.append(i)
+                break
+            i+=1
+    # print(arm_idx)
+    joint_pos_sample = zarr.array([joint_pos[s] for s in arm_idx])
+    joint_vel_sample = zarr.array([joint_vel[s] for s in arm_idx])
+    joint_torque_sample = zarr.array([joint_torque[s:s+20] for s in arm_idx])
+
+    # Resize Zarr (Action is the same as joint position)
+    action_arr.resize((action_arr.shape[0] + episode_len, action_arr.shape[1]))
+    action_arr[-episode_len:] = joint_pos_sample
+
+    joint_pos_arr.resize((joint_pos_arr.shape[0] + episode_len, joint_pos_arr.shape[1]))
+    joint_pos_arr[-episode_len:] = joint_pos_sample
+
+    joint_vel_arr.resize((joint_vel_arr.shape[0] + episode_len, joint_vel_arr.shape[1]))
+    joint_vel_arr[-episode_len:] = joint_vel_sample
+
+    joint_torque_arr.resize((joint_torque_arr.shape[0] + episode_len, joint_torque_arr.shape[1], joint_torque_arr.shape[2]))
+    joint_torque_arr[-episode_len:] = joint_torque_sample
 
 
-# # print(cnt)
-
-# data = np.load(os.path.join(root_path, "joint_vel.npy"))
-
-# cam_data_len = cam1_time.shape[0]
-# if cam1_time.shape[0] > cam2_time.shape[0]:
-#     print("camera shape not equal")
-#     cam1_data_len = cam2_time.shape[0]
-
-
-
-# cam1_arr = []
-# i = 0
-# arm_idx = []
-# for j in range(cam_data_len-1):
-#     while i < arm_time.shape[0]:
-#         if np.abs(cam1_time[j]-arm_time[i]) < 0.01:
-#             print("%f, %f, %f"%(cam1_time[j], arm_time[i], arm_time[i+20]))
-#             # data_arr = cam1_folder.create_dataset(str(i-1)+".1", shape=, chunks=(5,), dtype='f4')
-#             # cam1_arr.append()
-#             # cam2_arr.append()
-#             arm_idx.append(i)
-#             break
-#         i+=1
-
-# print(joint_torque.shape)
-# print(arm_idx)
 
 
 
